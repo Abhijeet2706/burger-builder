d6d655fec1b27533351d4e3355eca9f7750ea2c5
Abhijeet Kumar <abhijeetkumar2706@gmail.com>
2022-01-15 20:58:22 +0530

    Updates

---
+++ precache-manifest.434a306a83827e84c8c13776488b07b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aafd0611feac53d6d767e63273cecf5",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "4ee807ef4497b045bdc0",
+    "url": "/burger-builder/static/css/main.7844e147.chunk.css"
+  },
+  {
+    "revision": "c7b011edb8c7e1823f1b",
+    "url": "/burger-builder/static/js/2.6a4aba38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.6a4aba38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee807ef4497b045bdc0",
+    "url": "/burger-builder/static/js/main.a5ae99f4.chunk.js"
+  },
+  {
+    "revision": "68b0bcc03b01bbd962d6",
+    "url": "/burger-builder/static/js/runtime-main.79220b82.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
